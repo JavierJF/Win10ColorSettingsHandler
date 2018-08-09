3a03bcf5f68dab936e51ae79a5d1176d5b619fe1
Javier Jaramago Fernández <jaramago.fernandez.javier@gmail.com>
2018-08-09 17:35:18 +0200

    Initial commit with initial impl

---
+++ Win10ColorSettingsHandler/ColorManager.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Win10ColorSettingHandler {
+    [StructLayout(LayoutKind.Sequential)]
+    public struct DwmColorParams {
+        public uint ColorizationColor;
+        public uint ColorizationAfterglow;
+        public uint ColorizationColorBalance;
+        public uint ColorizationAfterglowBalance;
+        public uint ColorizationBlurBalance;
+        public uint ColorizationGlassReflectionIntensity;
+        public uint ColorizationOpaqueBlend;
+    };
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct ImmersiveColorPreference {
+        public uint crStartColor;
+        public uint crAccentColor;
+    }
+
+    public static class ColorManager {
+        // Functions for changing Windows window border colors
+        [DllImport("dwmapi.dll", EntryPoint = "#131", CallingConvention = CallingConvention.StdCall)]
+        private static extern int DwmpSetColorizationParameters(ref DwmColorParams dcpParams, bool alwaysTrue);
+        [DllImport("dwmapi.dll", EntryPoint = "#123")]
+        private static extern int DwmGetColorizationColor(out uint color, out bool blend);
+        [DllImport("dwmapi.dll", EntryPoint = "#127", CallingConvention = CallingConvention.StdCall)]
+        private static extern int DwmpGetColorizationParameters(out DwmColorParams dcpParams);
+
+        private static byte LowByte(UInt32 number) {
+            UInt32 mask = 0xFF;
+            UInt32 resNum = number & mask;
+
+            return (byte)resNum;
+        }
+
+        private static byte GetRValue(UInt32 number) {
+            byte rByte = LowByte(number);
+            return rByte;
+        }
+
+        private static byte GetGValue(UInt32 number) {
+            UInt32 shiftedUInt = number >> 8;
+            byte rByte = LowByte(shiftedUInt);
+
+            return rByte;
+        }
+
+        private static byte GetBValue(UInt32 number) {
+            UInt32 shiftedUInt = number >> 16;
+            byte rByte = LowByte(shiftedUInt);
+
+            return rByte;
+        }
+
+        public static UInt32 RGB(byte r, byte g, byte b) {
+            byte rVal = r;
+            UInt32 gVal = ((UInt32)g) << 8;
+            UInt32 bVal = ((UInt32)b) << 16;
+
+            UInt32 finalColor = r | gVal | bVal;
+
+            return finalColor;
+        }
+
+        public static int GetWindowBorderColors(out UInt32 color) {
+            DwmColorParams colorParams = new DwmColorParams {
+                ColorizationColor = 0,
+                ColorizationAfterglow = 0,
+                ColorizationColorBalance = 0,
+                ColorizationAfterglowBalance = 0,
+                ColorizationBlurBalance = 0,
+                ColorizationGlassReflectionIntensity = 0,
+                ColorizationOpaqueBlend = 0
+            };
+
+            var getRes = DwmpGetColorizationParameters(out colorParams);
+            UInt32 revColor = colorParams.ColorizationColor & 0x00FFFFFF;
+            UInt32 curColor = RGB(GetBValue(revColor), GetGValue(revColor), GetRValue(revColor));
+
+            color = curColor;
+
+            return getRes;
+        }
+
+        public static int SetWindowBorderColors(UInt32 color) {
+            DwmColorParams colorParams = new DwmColorParams {
+                ColorizationColor = 0,
+                ColorizationAfterglow = 0x00007fff,
+                ColorizationColorBalance = 0xedd816c5,
+                ColorizationAfterglowBalance = 0x0000002b,
+                ColorizationBlurBalance = 0x6987bf01,
+                ColorizationGlassReflectionIntensity = 0,
+                ColorizationOpaqueBlend = 0
+            };
+
+            var getRes = DwmpGetColorizationParameters(out colorParams);
+            if (getRes != 0) {
+                return getRes;
+            }
+
+            UInt32 rByte = (UInt32)GetRValue(color) << 16;
+            UInt32 gByte = (UInt32)GetGValue(color) << 8;
+            UInt32 bByte = GetBValue(color);
+
+            UInt32 dwNewColor = (UInt32)((((UInt32)0xC4) << 24) | (rByte | gByte | bByte));
+            colorParams.ColorizationColor = dwNewColor;
+            colorParams.ColorizationAfterglow = dwNewColor;
+            var setRes = DwmpSetColorizationParameters(ref colorParams, false);
+
+            return setRes;
+        }
+
+        // Functions for changing Windows accent colors
+        [DllImport("uxtheme.dll", CallingConvention = CallingConvention.StdCall)]
+        private static extern int GetUserColorPreference(out ImmersiveColorPreference cpcpPreference, bool fForceReload);
+        [DllImport("uxtheme.dll", EntryPoint = "#122", CallingConvention = CallingConvention.StdCall)]
+        private static extern int SetUserColorPreference(ref ImmersiveColorPreference cpcpPrefere, bool fForceCommit);
+
+        public static int GetAccentColor(out UInt32 color) {
+            ImmersiveColorPreference colorPreference = new ImmersiveColorPreference { crStartColor = 0, crAccentColor = 0 };
+            var res = GetUserColorPreference(out colorPreference, false);
+            color = colorPreference.crAccentColor & 0x00FFFFFF;
+
+            return res;
+        }
+
+        public static int SetAccentColor(UInt32 color) {
+            ImmersiveColorPreference colorPreference = new ImmersiveColorPreference { crStartColor = 0, crAccentColor = 0 };
+
+            color &= 0x00FFFFFF;
+            colorPreference.crAccentColor = color;
+            var res = SetUserColorPreference(ref colorPreference, true);
+
+            return res;
+        }
+    }
+}
---
+++ Win10ColorSettingsHandlerTests/ColorManagerTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Win10ColorSettingHandler;
+using NUnit.Framework;
+
+namespace Window10ColorSettingHandlerTests {
+    [TestFixture]
+    public class ColorManagerTests {
+        private UInt32 Orange = 0x00606848;
+
+        [Test]
+        public void TestGettingAccentColor() {
+            UInt32 currentColor = 0xffffffff;
+            var res = ColorManager.GetAccentColor(out currentColor);
+
+            Assert.IsTrue(currentColor != 0xffffffff && res == 0);
+        }
+
+        [Test]
+        public void TestSettingAccentColor() {
+            UInt32 oldColor = 0xffffffff;
+            var getRes = ColorManager.GetAccentColor(out oldColor);
+
+            Assert.AreEqual(0, getRes);
+
+            UInt32 newColor = 0xffffffff;
+            var setRes = ColorManager.SetAccentColor(0x000C63F7);
+
+            Assert.AreEqual(0, setRes);
+
+            getRes = ColorManager.GetAccentColor(out newColor);
+            Assert.AreEqual(0, setRes);
+            Assert.AreEqual((UInt32)0x000C63F7, newColor);
+
+            setRes = ColorManager.SetAccentColor(oldColor);
+            Assert.AreEqual(0, setRes);
+        }
+
+        [Test]
+        public void TestSettingWindowsBorderColor() {
+            UInt32 oldBorderColor = 0;
+            var getCurRes = ColorManager.GetWindowBorderColors(out oldBorderColor);
+            Assert.AreEqual(0, getCurRes);
+
+            UInt32 oldAccentColor = 0;
+            var getAccentRes = ColorManager.GetAccentColor(out oldAccentColor);
+            Assert.AreEqual(0, getAccentRes);
+
+            // This operations should always be done in sequence for border color
+            // being set properly
+            // ================================================================
+            var setAccentRes = ColorManager.SetAccentColor((UInt32)0x000c63f7);
+            Assert.AreEqual(0, setAccentRes);
+            var setBCRes = ColorManager.SetWindowBorderColors((UInt32)0x004343FF);
+            Assert.AreEqual(0, setBCRes);
+            // ================================================================
+
+            UInt32 borderColor = 0;
+            var getRes = ColorManager.GetWindowBorderColors(out borderColor);
+            Assert.AreEqual(0, getRes);
+            Assert.AreEqual((UInt32)0x004343FF, borderColor);
+
+            // Restore previous colors
+            // ================================================================
+            var restAccentColor = ColorManager.SetAccentColor(oldAccentColor);
+            Assert.AreEqual(0, restAccentColor);
+            var restoreRes = ColorManager.SetWindowBorderColors(oldBorderColor);
+            Assert.AreEqual(0, restoreRes);
+            // ================================================================
+        }
+    }
+}
